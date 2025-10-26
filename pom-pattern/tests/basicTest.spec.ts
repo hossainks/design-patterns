75377cf8f3dc1a5366d82397e096e689b007227a
hossainks <shakirhossain.qa@gmail.com>
2025-10-26 23:44:10 +0600

    Add basic tests

---
+++ pom-pattern/tests/basicTest.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from "@playwright/test";
+
+test("first test using simple Fixture", async ({ page }) => {
+  await page.goto("/");
+  await expect(page).toHaveTitle("Swag Labs");
+
+  await page.locator("#user-name").fill("standard_user");
+  await page.locator("#password").fill("secret_sauce");
+  await page.locator("#login-button").click();
+  await page.waitForTimeout(1000);
+
+  await expect(page).toHaveURL("https://www.saucedemo.com/inventory.html");
+  const header = page.locator(".app_logo");
+  await expect(header).toHaveText("Swag Labs");
+  await page.waitForTimeout(1000);
+});
